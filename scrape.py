3ff853372c67fca30b2a4e202a93ed6508f7ed12
Abe Stanway <abestanway@gmail.com>
2012-11-18 23:31:35 -0500

    Forever

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 import simplejson
 import requests
 import pymongo
+import time
 from pymongo import Connection
 
 # Mongo establish connection & define RDB
@@ -9,26 +10,21 @@ db = connection.nerd_rapper_names
 tweets = db.tweets
 
 def scrape(url):
-    losers = []
     for x in range(1,50):
         print x
         try:
             r = requests.get(url + str(x))
             json = simplejson.loads(r.content)
             for tweet in json['results']:
-                t = tweets.insert(tweet)
+                if tweets.find({'from_user' : tweet['from_user'], 'text' : tweet['text'], 'id' : tweet['id']}):
+                    continue
+                else:
+                  print "inserting"
+                  t = tweets.insert(tweet)
 
         except:
             print "Lost " + str(x)
-            losers.append(x)
 
-        for x in losers:
-            try:
-                r = requests.get(url + str(x))
-                json = simplejson.loads(r.content)
-                for tweet in json['results']:
-                    t = tweets.insert(tweet)
-            except:
-                print "Lost " + str(x)
-
-scrape('http://search.twitter.com/search.json?q=nerdrappernames&rpp=100&page=')
+while True:
+  scrape('http://search.twitter.com/search.json?q=nerdrappernames&rpp=100&page=')
+  time.sleep(500)
