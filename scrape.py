5b5e0f120efd001442853e1cd1b66c008166692b
Abe Stanway <astanway@etsy.com>
2013-03-02 22:32:27 +0000

    Wow tweets

--- scrape.py
+++ scrape.py
@@ -1,16 +1,19 @@
 import simplejson
 import requests
-import pymongo
+#import pymongo
 import time
-from pymongo import Connection
+import pickle
+#from pymongo import Connection
 
 # query
-query = "NerdRapperNames"
+query = "Wow. https://twitter.com/"
 
 # Mongo establish connection & define RDB
-connection = Connection()
-db = connection.nerds
-tweets = db.tweets
+#connection = Connection()
+#db = connection.nerds
+#tweets = db.tweets
+
+tweets = []
 
 def scrape(url):
     for x in range(1,50):
@@ -19,15 +22,21 @@ def scrape(url):
             r = requests.get(url + str(x))
             json = simplejson.loads(r.content)
             for tweet in json['results']:
-                if tweets.find({'from_user' : tweet['from_user'], 'text' : tweet['text'], 'id' : tweet['id']}).count() != 0:
-                    continue
-                else:
-                  print tweet['text']
-                  t = tweets.insert(tweet)
+              #  if tweets.find({'from_user' : tweet['from_user'], 'text' : tweet['text'], 'id' : tweet['id']}).count() != 0:
+              #      continue
+              #  else:
+                  tweets.append(tweet)
+                  #t = tweets.insert(tweet)
 
         except:
             print "Lost " + str(x)
 
+run = 0
 while True:
   scrape('http://search.twitter.com/search.json?q=' + query + '&rpp=100&page=')
+  output = open('tweets' + str(run), 'wb')
+  pickle.dump(tweets, output)
+  output.close()
+  run += 1
+  tweets[:] = []
   time.sleep(500)
